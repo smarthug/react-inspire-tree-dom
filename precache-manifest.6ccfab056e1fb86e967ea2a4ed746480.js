f0fdc301fa31ef6d1053e574f29829e3647862c0
smarthug <kirklayer@gmail.com>
2020-07-10 15:21:07 +0900

    Updates

---
+++ precache-manifest.6ccfab056e1fb86e967ea2a4ed746480.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cde522fe6bbaf0f6221c50713c9926a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9b3a49fb5b7a5a17b7f8",
+    "url": "./static/css/main.e94ec0a9.chunk.css"
+  },
+  {
+    "revision": "3df81bc26c0faf346591",
+    "url": "./static/js/2.caaeae28.chunk.js"
+  },
+  {
+    "revision": "b695e190d674943163f4cef90aef5336",
+    "url": "./static/js/2.caaeae28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b3a49fb5b7a5a17b7f8",
+    "url": "./static/js/main.efc42d65.chunk.js"
+  },
+  {
+    "revision": "59bd054f3bde6b0e219b610549d13f6f",
+    "url": "./static/js/main.efc42d65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34bb265e230ca84830e1",
+    "url": "./static/js/runtime-main.ca623935.js"
+  }
+]);
